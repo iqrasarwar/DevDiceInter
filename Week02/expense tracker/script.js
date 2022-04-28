eaa03357540c35568ddf9b7609d28d9000f17e1f
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-04-28 23:17:19 +0500

    expense trcker completed

--- Week02/expense tracker/script.js
+++ Week02/expense tracker/script.js
@@ -11,4 +11,38 @@ function newTrans()
    }
    let body = document.getElementsByTagName("tbody")[0];
    body.innerHTML += "<tr><td>"+text+"</td><td>"+amount+"</td></tr>";
+   let rows = document.getElementById("tbl").rows;
+   let income = 0,blnce =0, expense =0;
+   for (let index = 1; index < tbl.length; index++)
+   {
+      let x = parseInt(rows[index].cells[1].innerHTML);
+      blnce += x;
+      if(x > 0)
+      income+=parseInt(x);
+      else
+      expense+=parseInt(x);
+   }
+   document.getElementsByClassName("expense")[0].innerHTML = "$"+expense;
+   document.getElementsByClassName("income")[0].innerHTML = "$"+income;
+   document.getElementsByClassName("balance")[0].innerHTML = "$"+blnce;
+   document.getElementById("text").value = "";
+   document.getElementById("amount").value = "";
+}
+let y = document.getElementsByClassName("hist")[0];
+y.onclick = fun;
+function fun()
+{
+   if( document.getElementsByClassName("toHide")[0].style.display == "none")
+   document.getElementsByClassName("toHide")[0].style.display = ""
+   else
+   document.getElementsByClassName("toHide")[0].style.display = "none"
+}
+let z = document.getElementsByClassName("trans")[0];
+z.onclick = trns;
+function trns()
+{
+   if( document.getElementsByClassName("toHide1")[0].style.display == "")
+   document.getElementsByClassName("toHide1")[0].style.display = "none"
+   else
+   document.getElementsByClassName("toHide1")[0].style.display = ""
 }
