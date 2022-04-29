31dbc0945ee258bdad7ce1b08ebc76db2e3a16d9
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-04-29 20:47:42 +0500

    tto do v2

--- Week02/todo-list/script.js
+++ Week02/todo-list/script.js
@@ -2,11 +2,18 @@ let btn = document.getElementsByTagName("button")[0];
 btn.onclick = next;
 function next()
 {
-   let x = document.getElementById("tbody");
-   console.log(x)
+   let x = document.getElementById("todo");
    let inputvl = document.getElementsByTagName("input")[0].value;
    if(inputvl != "")
-   x.innerHTML += "<tr><td>"+inputvl+"</td><td onclick ='deleteItem()'><i class='fa fa-times' aria-hidden='true'></i></td></tr>"
+   x.innerHTML += "<div class='tsk-col'>  <p>"+inputvl+"</p>   <p>   <p class='del'> <i class='fa fa-trash' aria-hidden='true'></i></p>  <p><i class='fa fa-arrow-right' aria-hidden='true'></p>  </p>  </div>"
 }
+let item = document.getElementsByClassName("del");
+for (let i = 0; i < item.length; i++) {
+   const element = item[i];
+   element.addEventListener("click", e =>
+   {
+      console.log(e.traget)
+   })
 
+}
 
