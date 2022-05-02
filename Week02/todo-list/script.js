a437dcf6b6aabc8667480f9b667926f5da9ec52b
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-03 01:35:34 +0500

    to do list task compl

--- Week02/todo-list/script.js
+++ Week02/todo-list/script.js
@@ -4,16 +4,38 @@ function next()
 {
    let x = document.getElementById("todo");
    let inputvl = document.getElementsByTagName("input")[0].value;
+   document.getElementsByTagName("input")[0].value = "";
    if(inputvl != "")
-   x.innerHTML += "<div class='tsk-col'>  <p>"+inputvl+"</p>   <p>   <p class='del'> <i class='fa fa-trash' aria-hidden='true'></i></p>  <p><i class='fa fa-arrow-right' aria-hidden='true'></p>  </p>  </div>"
-}
-let item = document.getElementsByClassName("del");
-for (let i = 0; i < item.length; i++) {
-   const element = item[i];
-   element.addEventListener("click", e =>
    {
-      console.log(e.traget)
-   })
-
+      if(x.innerHTML == "To Do Tasks appears here!") x.innerHTML="";
+      x.innerHTML += "<div class='tsk-col'>  <p>"+inputvl+"</p>   <p>   <p class='del'> <i class='fa fa-trash' aria-hidden='true'></i></p>  <p class='InProg'><i class='fa fa-arrow-right' aria-hidden='true'></i></p>  </p>  </div>";
+   }
+   let item = document.getElementsByClassName("InProg");
+   let dele = document.getElementsByClassName("del");
+   for (let i = 0; i < item.length; i++) {
+      item[i].onclick = (e) => inprog(e,"inprogress");
+   }
+   for (let i = 0; i < dele.length; i++) {
+      dele[i].onclick = (e) => del(e);
+   }
 }
 
+function inprog(e,str)
+{
+   let y=e.target.parentElement.parentElement.firstElementChild.innerHTML;
+   document.getElementById(str).insertAdjacentHTML("beforeend",
+   "<div class='tsk-col'>  <p>"+y+"</p>   <p>   <p class='dele'> <i class='fa fa-trash' aria-hidden='true'></i></p>  <p class='comp'><i class='fa fa-arrow-right' aria-hidden='true'></i></p>  </p>  </div>");
+   e.target.parentElement.parentElement.remove();
+   let del1 = document.getElementsByClassName("dele");
+   let comp = document.getElementsByClassName("comp");
+   for (let i = 0; i < del1.length; i++) {
+      del1[i].onclick = (e) => del(e);
+   }
+   for (let i = 0; i < comp.length; i++) {
+     comp[i].onclick = (e) => inprog(e,"complete");
+   }
+}
+function del(e)
+{
+   e.target.parentElement.parentElement.remove();
+}
