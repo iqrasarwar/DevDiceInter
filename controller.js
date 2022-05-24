091ccbdc9462ecc5a3373488bb810d55d2750dd1
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-24 23:42:52 +0500

    user crud

---
+++ controller.js
@@ -0,0 +1,44 @@
+function deleteUser()
+{
+   let id = document.getElementById("NameToDel").value;
+   const xhttp = new XMLHttpRequest();
+   xhttp.open("DELETE", `http://localhost:3000/user/${id}`, false);
+   xhttp.send();
+   location.reload();
+}
+
+function listUsers()
+{
+   let x = "";
+   const xhttp = new XMLHttpRequest();
+   xhttp.open("GET", `http://localhost:3000/users`, false);
+   xhttp.send();
+   const users = JSON.parse(xhttp.responseText);
+   for(let user of users)
+   {
+      x+=`<div><p>${user.name} ${user.password} ${user.role}</p><br><button id="${user.name}" onclick ="edit()">Edit</button></div>`
+   }
+
+   document.getElementById("listt").innerHTML += x;
+}
+
+listUsers();
+
+function edit(e)
+{
+   console.log(e);
+   const xhttp = new XMLHttpRequest();
+   xhttp.open("GET", `http://localhost:3000/users`, false);
+   xhttp.send();
+   const users = JSON.parse(xhttp.responseText);
+   // for(let user of users)
+   // {
+   //    if(user.name == name)
+   //    {
+   //       document.getElementById("editn").value = user.name;
+   //       document.getElementById("editp").value = user.password;
+   //       document.getElementById("editr").value = user.role;
+   //    }
+   // }
+   // document.getElementById("editform").action = `http://localhost:3000/user/${name}`
+}
--- index.js
+++ index.js
@@ -2,25 +2,27 @@ const express = require("express")
 const app = express()
 const jwt = require("jsonwebtoken")
 const bodyparser = require("body-parser")
+const cors = require('cors')
 const accessTokensecrete = "dfjgrodnojl32905itngf950tiojgog"
-const users = [
+let users = [
    {
       name : 'john',
       password:"pass1",
       role:"admin"
    },
    {
-      name : 'john',
-      password:"pass1",
-      role:"admin"
+      name : 'johny',
+      password:"pass2",
+      role:"user"
    }
 ]
 
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.listen(3000,()=>{
    console.log("authentication")
 })
-
+app.use(cors());
 app.post("/login",(req,res) =>
 {
    const {name,password} = req.body;
@@ -41,3 +43,41 @@ app.post("/login",(req,res) =>
       res.send("username password incorrect");
    }
 });
+
+app.post('/user', (req, res) => {
+   const user = req.body;
+   console.log(user);
+   users.push(user);
+   res.send('user added');
+});
+
+app.get('/users', (req, res) => {
+   res.json(users);
+});
+
+app.delete('/user/:name', (req, res) => {
+   const name = req.params.name;
+   users = users.filter(i => {
+      if (i.name !== name) {
+          return true;
+      }
+      return false;
+  });
+  res.send('user is deleted');
+});
+
+app.post('/user/:name', (req, res) => {
+   const name = req.params.name;
+   const newuser = req.body;
+
+   for (let i = 0; i < users.length; i++) {
+       let user = users[i]
+       if (user.name === name) {
+           users[i] = newuser;
+       }
+   }
+   res.send('user is edited');
+});
+
+//use lod sh
+//mke hsh
